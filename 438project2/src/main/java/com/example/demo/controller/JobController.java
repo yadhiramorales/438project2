5eecf9ea64bec6ba5b2418f6d416f064b85ec84f
gRome0 <gmartinezromero@csumb.edu>
2026-03-05 01:42:13 -0800

    This is the repo for the JobNote Repo

--- 438project2/src/main/java/com/example/demo/controller/JobController.java
+++ 438project2/src/main/java/com/example/demo/controller/JobController.java
@@ -11,7 +11,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.JobApplication;
-import com.example.demo.repository.JobApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
