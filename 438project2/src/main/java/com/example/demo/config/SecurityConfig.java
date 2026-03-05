d265beace1fb04a154a7b859cd758b653601131c
yadhiramorales <yadhimorales45@gmail.com>
2026-03-04 17:55:56 -0800

    gradle updates

--- 438project2/src/main/java/com/example/demo/config/SecurityConfig.java
+++ 438project2/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
