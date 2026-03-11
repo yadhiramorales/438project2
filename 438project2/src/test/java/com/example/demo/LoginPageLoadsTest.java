155d36d91ac1ae4d20efacf8512640f9b0660ab1
Aidan Shanley <ashanley@csumb.edu>
2026-03-11 11:40:22 -0700

    New tests successfully made for the application

---
+++ 438project2/src/test/java/com/example/demo/LoginPageLoadsTest.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LoginPageLoadsTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void loginPageLoads() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+}
